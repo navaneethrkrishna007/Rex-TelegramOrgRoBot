e1869ca93c7d53ec76c7baea634906a2a6ee5c24
REX BOTZ <89206214+REX-BOTZ@users.noreply.github.com>
2021-12-26 12:25:23 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# Rex-TelegramOrgRoBot
+### My Telegram Org Bot
+
+
+## Deploy to Heroku👇
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/REX-BOTZ/Telegram-Org/tree/main)
+
+
+</a>
+    <a href="https://telegram.me/Rex_Bots_Support">
+        <img
+            src="https://img.shields.io/badge/Support Group-blue?&style=for-the-badge&logo=Telegram"
+            alt="Telegram"
+        >
+
+
+
+</a>
+    <a href="https://telegram.me/REX_BOTZ">
+        <img
+            src="https://img.shields.io/badge/Updates Channel-blue?&style=for-the-badge&logo=Telegram"
+            alt="Telegram"
+        >
---
+++ translation.py
@@ -0,0 +1,20 @@
+class Translation(object):
+    START_TEXT = """Hi!
+Hai Iam a Simple My.telegram.org Bot.
+
+To Get API ID & API HASH Enter your Telegram Phone Number With Country Code.
+
+🤖 𝙱𝚘𝚝 𝚄𝚙𝚍𝚊𝚝𝚎𝚜 : @Rex_Botz
+
+Click /Start To Restart The Progress"""
+    AFTER_RECVD_CODE_TEXT = """I see!
+Now please send the Telegram code that you received from Telegram!
+
+This code is only used for the purpose of getting the APP ID from my.telegram.org
+
+Click /Start To Restart The Progress"""
+    BEFORE_SUCC_LOGIN = "recieved code. Scarpping web page ..."
+    ERRED_PAGE = "something wrongings. failed to get app id. \n\n@Rex_Bots_Support\n\nHow Get Api Code For Website"
+    CANCELLED_MESG = "Bye! Please re /start the bot conversation"
+    IN_VALID_CODE_PVDED = "Send me the code that you received from Telegram"
+    IN_VALID_PHNO_PVDED = "Hey, Send me your Phone Number"
